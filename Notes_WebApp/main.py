c59b962c280d2629ef29c2ed9a32b52c075903b6
Sai Krishna Dhabole <sdhabole@primesoft.net>
2023-10-04 18:40:05 +0530

    Added Dockerfile

--- Notes_WebApp/main.py
+++ Notes_WebApp/main.py
@@ -1,6 +1,12 @@
 from website import create_app
+import sys
+
+# Check if at least one command-line argument is provided
+if len(sys.argv) < 2:
+    print("No Port Number found -- Usage: python main.py <port_number>")
+    sys.exit(1)
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True , port=1234 , host="0.0.0.0")
+    app.run(debug=True , port=sys.argv[1] , host="0.0.0.0")
